7ccaeaf884dbc18fe5c7f7d14fafc18b5b075d62
Alex <athanas1@my.wctc.edu>
2021-02-20 15:06:42 -0600

    Add nlog, psuedocode start

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NLog.Web;
+
+
+namespace MovieLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie = new Movie();
+            Menu menu = new Menu();
+            File file = new File();
+
+            file.addFile("movies.csv");
+            Menu.display();
+
+            do{
+            var choice = menu.GetInput();
+            
+
+             } while(choice == "1"|| choice == "2");
+        }
+    }
+}
